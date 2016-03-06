075464f0a64704db3cc8dff175e694e534c165d6
almat <aaitzhanov@gmail.com>
2016-03-05 22:17:39 -0800

    03/05-3

--- src/task554/Main.java
+++ src/task554/Main.java
@@ -5,17 +5,21 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-/**
- * Created by almat on 2/26/16.
- */
 public class Main {
-
-    public static void main(String[] args) {
+    public static void main(String args[]) throws Exception {
         init(System.in, System.out);
     }
 
-    public static void init(InputStream in, OutputStream out) {
+    public static void init(InputStream in, OutputStream out) throws Exception {
         Scanner sc = new Scanner(in);
-        PrintWriter pw = new PrintWriter(out);
+        PrintWriter pr = new PrintWriter(out);
+        int n = sc.nextInt();
+        int[] a = new int[n+1];
+//        int s = n*(n+1)/2 +1;
+        a[0] = 1;
+        for (int i=1; i<=n; i++)
+            a[i] = a[i-1] + i;
+        pr.print(a[n]);
+        pr.flush();
     }
 }
